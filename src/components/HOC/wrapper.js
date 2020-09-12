9f3886f2710de0360b9ef93ddb53f78cee299df8
Yogesh Bansiwal <YogeshBansiwal@outlook.com>
2020-09-13 00:51:57 +0530

    removed class shadow class from HOC

--- src/components/HOC/wrapper.js
+++ src/components/HOC/wrapper.js
@@ -3,12 +3,10 @@ const container = (props) => {
   return (
     <div class="row">
       <div class="col-lg-12 mb-4">
-        <div class="card shadow mb-4">
-          <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">{props.heading}</h6>
-          </div>
-          {props.children}
+        <div class="card-header py-3">
+          <h6 class="m-0 font-weight-bold text-primary">{props.heading}</h6>
         </div>
+        {props.children}
       </div>
     </div>
   );
