4780713a8dd1e8c8cae7a2fed09d37480e0da8fe
Yogesh Bansiwal <YogeshBansiwal@outlook.com>
2020-09-13 00:59:34 +0530

    relative path in index.html for css

--- src/components/HOC/wrapper.js
+++ src/components/HOC/wrapper.js
@@ -1,10 +1,10 @@
 import React from "react";
 const container = (props) => {
   return (
-    <div class="row">
-      <div class="col-lg-12 mb-4">
-        <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">{props.heading}</h6>
+    <div className="row">
+      <div className="col-lg-12 mb-4">
+        <div className="card-header py-3">
+          <h6 className="m-0 font-weight-bold text-primary">{props.heading}</h6>
         </div>
         {props.children}
       </div>
